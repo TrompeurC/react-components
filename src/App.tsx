067a486ab3f7185f8db78745b231e2083e728e8b
TrompeurC <13604811047@163.com>
2024-04-09 00:17:35 +0800

    feat: calendar demo组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React, { memo } from "react";
+import Calendar from "./components/calendar";
 
 const App = memo(() => {
   return (
     <div>
-      app
+      <Calendar/>
     </div>
   );
 });
