ed641538fb2db65731eb6b77ce4aa524868c7ba6
TrompeurC <13604811047@163.com>
2024-04-16 21:05:51 +0800

    feat: space 组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,30 @@
 import { memo } from "react";
-import CalendarTest from "./components/Calendar/CalendarTest";
-import { IconAdd } from "./components/Icon/icons/IconAdd";
-import { IconEmail } from "./components/Icon/icons/IconEmail";
+import Space from "./components/Space";
+import './App.scss'
+import { ConfigContext } from "./components/Space/ConfigProvider";
 
 const App = memo(() => {
-  
+
   return (
     <div>
-      <IconAdd></IconAdd>
-      <IconEmail></IconEmail>
+      <ConfigContext.Provider value={{ space: { size: 20 } }}>
+        <Space direction="horizontal">
+          <div className="box"></div>
+          <div className="box"></div>
+          <div className="box"></div>
+        </Space>
+        <Space direction="vertical">
+          <div className="box"></div>
+          <div className="box"></div>
+          <div className="box"></div>
+        </Space>
+      </ConfigContext.Provider>
+      <hr />
+      <div>
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+      </div>
     </div>
   );
 });
